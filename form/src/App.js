878dd340a73500ba61ce5dba886b7e27c4b29144
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2021-11-01 09:07:32 +0000

    Initial commit

---
+++ form/src/App.js
@@ -0,0 +1,12 @@
+import "./app.css";
+import FormInput from "./components/FormInput";
+
+const App = () => {
+  return <div className="App"> 
+  <form>
+   <FormInput/>
+  </form>
+  </div>;
+
+};
+export default App;
---
+++ form/src/components/FormInput.jsx
@@ -0,0 +1,12 @@
+import "./formInput.css";
+
+const FormInput = () => {
+  return (
+    <div className="formInput"> 
+    <label>Username</label>
+    <input/>
+  </div>
+  )
+
+};
+export default FormInput;
